7589adb53da9eef5b78078a557148226b3c69689
fuguoqiang <434543162@qq.com>
2022-05-08 15:27:53 +0800

    pushNamed

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -17,14 +17,7 @@ class Screen0 extends StatelessWidget {
               color: Colors.red,
               child: Text('Go To Screen 1'),
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Screen1();
-                    },
-                  ),
-                );
+                Navigator.pushNamed(context, '/first');
                 //Navigate to Screen 1
               },
             ),
@@ -33,14 +26,7 @@ class Screen0 extends StatelessWidget {
               child: Text('Go To Screen 2'),
               onPressed: () {
                 //Navigate to Screen 2
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Screen2();
-                    },
-                  ),
-                );
+                Navigator.pushNamed(context, '/second');
               },
             ),
           ],
